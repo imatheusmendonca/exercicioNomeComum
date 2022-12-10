12e46fe0ab16ef304feb9945fbacf4173776470b
imatheusmendonca <120023913+imatheusmendonca@users.noreply.github.com>
2022-12-10 11:13:15 -0300

    Atividade para digitar nome e tocar no botao

---
+++ exercicio2dia9dez/exercicio2dia9dez/Form1.cs
@@ -0,0 +1,30 @@
+namespace exercicio2dia9dez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = " O nome é: comum";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = " O nome é: diferente";
+        }
+    }
+}
